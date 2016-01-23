132a8a09ab74f3ef972534e9c5c6367883c7afed
nic <netzeln@gmail.com>
2016-01-22 17:05:18 -0800

    be a breaking point before going home for the weekend

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,20 @@ Order.prototype.toppingsNumber =function(){
   return this.toppings.length;
 }
 
+// Order.prototype.sizeCost =function(){
+//  var pieCost;
+//  function(){
+//   if (this.sizePie() = "large"){
+//     return = 12;
+//   }else if (this.sizePie() = "xLarge"){
+//     return = 14;
+//   }
+//   // return pieCost;
+//   }
+// }
+// var orderCost = function(Order)){
+//   var
+// }
 $(document).ready(function() {
 
 
--- spec/specs.js
+++ spec/specs.js
@@ -1,15 +1,27 @@
 describe('Order', function(){
   it("takes the order information and writes it to the order", function(){
-    var testPizza = new Order("large",["cheese", "onion"],"1");
-    expect(testPizza.sizePie).to.equal("large");
+    var testPizza = new Order("xLarge",["cheese", "onion"],"1");
+    expect(testPizza.sizePie).to.equal("xLarge");
     expect(testPizza.toppings).to.eql(["cheese", "onion"]);
     expect(testPizza.numberPizzas).to.equal("1");
   });
 
   it("adds the toppingsNumber proptotype to the Order object", function(){
-    var testPizza = new Order("large",["cheese", "onion"],"1");
+    var testPizza = new Order("xLarge",["cheese", "onion"],"1");
     expect(testPizza.toppingsNumber()).to.equal(2);
+  });
+
+  it("adds the sizeCost method prototype to the order object", function(){
+    var testPizza = new Order("xLarge",["cheese", "onion"],"1");
+    expect(testPizza.sizeCost()).to.equal(14);
   });
 });
 
-describe
+
+
+// describe("orderCost", function(){
+//   var testPizza = new Order("large",["cheese", "onion"],"1");
+//   it("sets the base cost of the pizza by size",function(){
+//     expect(orderCost(sizePie)).to.equal(12);
+//   })
+// })
