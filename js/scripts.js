cb260c849bb6e2065ad34b2433e5c508d0c1c057
Netzel <Netzel>
2016-01-26 19:50:50 -0800

    "make a mess of things before it's time to go to sleep"

--- js/scripts.js
+++ js/scripts.js
@@ -1,80 +1,96 @@
 
-function Pizza(sizePie, toppings, numberPizzas){
+function Pizza(sizePie, toppings, numberPizzas, pizzasCost){
   this.sizePie = sizePie;
   this.toppings = toppings;
   this.numberPizzas = numberPizzas;
+//I know it's wrong to have this here. I tried very hard to have it be a prototype, but I coudn't make two prototypes talk to each other, or figure out how to write the "expect" statements.
+  this.pizzasCost = function(){
+      var pieCost;
+        if (this.sizePie === "xLarge"){
+          pieCost =  14;
+        } else if (this.sizePie === "large"){
+          pieCost = 12;
+        } else if (this.sizePie === "medium"){
+          pieCost = 10;
+        } else if(this.sizePie === "small"){
+          pieCost = 8;
+        } else {
+          pieCost = 0;
+        }
+        return (pieCost + (this.toppings.length / 2)) * this.numberPizzas;
+  }
 }
 
 
-Pizza.prototype.pizzaCost = function(){
-    var pieCost;
-      if (this.sizePie === "xLarge"){
-        pieCost =  14;
-      } else if (this.sizePie === "large"){
-        pieCost = 12;
-      } else if (this.sizePie === "medium"){
-        pieCost = 10;
-      } else if(this.sizePie === "small"){
-        pieCost = 8;
-      } else {
-        pieCost = 0;
-      }
-      return (pieCost + (this.toppings.length / 2)) * this.numberPizzas;
-}
+// Pizza.prototype.pizzaCost = function(){
+//     var pieCost;
+//       if (this.sizePie === "xLarge"){
+//         pieCost =  14;
+//       } else if (this.sizePie === "large"){
+//         pieCost = 12;
+//       } else if (this.sizePie === "medium"){
+//         pieCost = 10;
+//       } else if(this.sizePie === "small"){
+//         pieCost = 8;
+//       } else {
+//         pieCost = 0;
+//       }
+//       return (pieCost + (this.toppings.length / 2)) * this.numberPizzas;
+// }
 
-Pizza.prototype.addPizza = function(){
- return yourOrder.pizzasOrdered.push(this);
-
-
-}
-
-function Order(name, address, methodPayment, pizzasOrdered){
+function Order(name, address, pizzasOrdered){
       this.name = name;
       this.address = address;
-      this.methodPayment = methodPayment;
       this.pizzasOrdered =[];
   }
 
-  Order.prototype.totalCost = function(){
-    var costOrder;
-    var pizzasInOrder = yourOrder.pizzasOrdered;
-    pizzasInOrder.forEach(function(pizza){
-      costOrder = costOrder + pizza.pizzaCost();
-    });
-    return costOrder;
-  }
+  // Order.prototype.totalCost = function(){
+  //   var costOrder = 0;
+  //
+  //   console.log(pizzasOrdered);
+  //     this.pizzasOrdered.forEach(function(pizza){
+  //       costOrder += testOrder.pizzasCost;
+  //   });
+  //   return costOrder;
+  // }
 
-  Order.prototype.addPizza = function(){
-   return this.pizzasOrdered.push();
-var yourOrder = new Order();
 
-// $(document).ready(function() {
+
+// var yourOrder = new Order();
+
+$(document).ready(function() {
+
+
+$("form.getInfo").submit(function(event){
+  event.preventDefault();
+  var yourName = $("#name").val();
+  var yourAddress = $("form#address").val();
+  var yourPies = [];
+  var yourOrder = new Order(yourName, yourAddress, yourPayment, yourPies);
+console.log(yourName);
+console.log(yourAddress);
+console.log(yourPies);
+console.log(yourOrder);
+});
+
+  $("form.pizzaCreator").submit(function(event){
+    event.preventDefault();
+    var selectedSize = $("#pieSize").val();
+    var selectedToppings = [];
+        $("input[name=toppingCheck[]']:checked").map(function(){
+            selectedToppings.push($(this).val());
+          });
+    var selectedNumber = $("input#numberPies").val();
+    var newPizza = new Pizza(selectedSize, selectedToppings, selectedNumber);
+    console.log(newPizza)
+  yourPies.push(newPizza);
+  //would like to have  pizza added to order displayed. maybe as a list
+  });
 //
-// var yourOrder = new Order(yourName, yourAddress, yourPayment, yourPies);
-//
-//   $("form.pizzaCreator")submit(function(event){
-//     event.preventDefault();
-//     var selectedSize = $("input#pieSize").val();
-//     var selectedToppings = [];
-        // $("input[name=toppingCheck[]']:checked").map(function(){
-        //     selectedToppings.push($(this).val());
-        //   };
-//     var selectedNumber = $("input#numberPies").val();
-//     var newPizza = new Pizza(selectedSize, selectedToppings, selectedNumber);
-//    yourOrder.pizzasOrdered.push(newPizza);
-//   //would like to have  pizza added to order displayed. maybe as a list
-//   });
-// //
-//   $("form.getInfo")submit(function(event){
-//     event.preventDefault();
-//     var yourName = $("form#name").val();
-//     var yourAddress = $("form#address").val();
-//     var yourPayment = $("form#payment").val();
-//     var yourPies = [];
-//   });
+
 // //
 // //
 // // //would like to display total order cost if possible as the orders are added
 // // //grab names and delivery address to the order
 // //
-// });
+ });
--- spec/specs.js
+++ spec/specs.js
@@ -1,43 +1,50 @@
 describe('Pizza', function(){
   it("creates information for a new pizza", function(){
-    var testPizza = new Pizza("xLarge",["cheese", "onion"],"1");
+    var testPizza = new Pizza("xLarge",["cheese", "onion"],"2");
     expect(testPizza.sizePie).to.equal("xLarge");
     expect(testPizza.toppings).to.eql(["cheese", "onion"]);
-    expect(testPizza.numberPizzas).to.equal("1");
-  });
-
-
-
-  it("adds the pizzaCost prototype to the order object", function(){
-    var testPizza = new Pizza("medium",["cheese", "onion", "pepperoni"],"3");
-    expect(testPizza.pizzaCost()).to.equal(34.50);
-  });
-
-  it("adds the addPizza method prototype to the Order object",function(){
-    var testPizza = new Pizza("medium",["cheese", "onion", "pepperoni"],"3");
-  var testOrder = new Order("Timmy", "123 Sesame St", "cash", [testPizza])
-    expect(testPizza.addPizza()).to.eql({testPizza});
+    expect(testPizza.numberPizzas).to.equal("2");
+    expect(testPizza.pizzasCost()).to.equal(30);
 
   });
+
+
+
+//   it("adds the pizzaCost prototype to the order object", function(){
+//     var testPizza = new Pizza("medium",["cheese", "onion", "pepperoni"],"3");
+//     expect(testPizza.pizzaCost()).to.equal(34.50);
+//   });  //
+//
 });
 
 describe ('Order', function(){
   it("adds information about the order including name, address and the pizzas ordered", function(){
     var testPizza = new Pizza("medium",["cheese", "onion", "pepperoni"],"3");
     var testPizza2 = new Pizza("xLarge",["cheese"],"1");
-    var testOrder = new Order("Timmy", "123 Sesame St", "cash", []);
+    var testOrder = new Order("Timmy", "123 Sesame St", []);
     expect(testOrder.name).to.equal("Timmy");
     expect(testOrder.address).to.equal("123 Sesame St");
-    expect(testOrder.methodPayment).to.equal("cash");
+    
     expect(testOrder.pizzasOrdered).to.eql([]);
   });
 
+  // it("adds the totalCost method prototype to the Order object",function(){
+  //   var testPizza = new Pizza("medium",["cheese", "onion"],2);
+  //   var testPizza2 = new Pizza("small", ["mushroom", "onion"], 1 );
+  //   var testOrder = new Order("Timmy", "123 Sesame St", "cash", [testPizza, testPizza2]);
+  //   expect(testOrder.totalCost()).to.equal(31);
+// describe ("pushPizza", function(){
+//   it("pushes a Pizza Object into Order.PizzasOrdered", function(){
+//     var testPizza = new Pizza("medium",["cheese", "onion", "pepperoni"],"3");
+//     var testOrder = new Order("Timmy", "123 Sesame St", "cash", []);
+//     expect(testOrder.pizzasOrdered[0]).to.eql(testPizza);
+//   });
+// });
 
-  it("adds the totalCost method prototype to the Order object",function(){
-    var testPizza = new Pizza("medium",["cheese", "onion"],2);
-    var testPizza2 = new Pizza("small", ["mushroom", "onion"], 1 );
-    var yourOrder = new Order("Timmy", "123 Sesame St", "cash", [testPizza, testPizza2]);
-    expect(yourOrder.totalCost()).to.equal(31);
 
-  });
 });
+
+// it("adds the addPizza method prototype to the Order object",function(){
+//   var testPizza = new Pizza("medium",["cheese", "onion", "pepperoni"],"3");
+// var testOrder = new Order("Timmy", "123 Sesame St", "cash", [testPizza])
+//   expect(testPizza.addPizza()).to.eql({testPizza});
