984f79148ecb362af95b63d073029447aecb9de9
nic <netzeln@gmail.com>
2016-01-27 11:08:38 -0800

    get order.total cost prototype to work (sadly without running spec Tests)

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,8 @@
 function Pizza(sizePie, toppings, numberPizzas){
   this.sizePie = sizePie;
   this.toppings = toppings;
-  this.numberPizzas = numberPizzas;
+  this.numberPizzas = parseInt(numberPizzas);
+  this.cost=0;
 }
 
 
@@ -22,10 +23,10 @@ Pizza.prototype.pizzaCost = function(){
       return (pieCost + (this.toppings.length / 2)) * this.numberPizzas;
 }
 
-Pizza.prototype.addPizza = function(){
- yourOrder.pizzasOrdered.push(this);
- return yourOrder.pizzasOrdered;
-}
+// Pizza.prototype.addPizza = function(){
+//  yourOrder.pizzasOrdered.push(this);
+//  return yourOrder.pizzasOrdered;
+// }
 
 function Order(name, address, pizzasOrdered){
       this.name = name;
@@ -35,50 +36,87 @@ function Order(name, address, pizzasOrdered){
   }
 
   Order.prototype.totalCost = function(){
-    var costOrder = [];
-    // var pizzasInOrder = yourOrder.pizzasOrdered;
-    yourOrder.pizzasOrdered.forEach(function(pizza){
-      // costOrder = parseInt(costOrder) + this.pizzaCost();
-        costOrder.push(pizza.pizzaCost());
-      console.log(costOrder);
-    });
+    var costOrder = 0;
+   this.pizzasOrdered.forEach(function(Pizza){
+     costOrder = costOrder + Pizza.cost;
+   });
+    // $(this.pizzasOrdered).each(function(pizza){
+    //   costOrder = costOrder + pizza.cost.val();
+    //   console.log(costOrder);
+    // });
     return costOrder;
   }
-// var yourOrder = new Order();
+
 
 $(document).ready(function() {
-
-// var yourOrder = new Order(yourName, yourAddress, yourPayment, yourPies);
 
   $("form#getOrder").submit(function(event){
     event.preventDefault();
     var yourName = $("input#order-name").val();
     var yourAddress = $("input#address").val();
     // var yourPayment = $("form#payment").val();
+    var yourOrder = new Order(yourName, yourAddress, yourPies);
     var yourPies = [];
-    var selectedSize = $("#pieSize").val();
-    var selectedToppings = []
-    $("input[name='toppingCheck']:checked").each(function ()
-      {
-            selectedToppings.push($(this).val());
+
+    $(".pizza-Creator").each(function(){
+      var selectedSize = $(this).find("#pieSize").val();
+      var selectedToppings = []
+        $(this).find("input[name='toppingCheck']:checked").each(function (){
+        selectedToppings.push($(this).val());
+        });
+      var selectedNumber = $(this).find("#numberPies").val();
+      var newPizza = new Pizza(selectedSize, selectedToppings, selectedNumber);
+        yourOrder.pizzasOrdered.push(newPizza);
+        newPizza.cost = newPizza.pizzaCost();
+        console.log(newPizza);
+        console.log(newPizza.pizzaCost());
       });
 
 
 
-    var selectedNumber = $("#numberPies").val();
-    var yourOrder = new Order(yourName, yourAddress, yourPies);
-    var newPizza = new Pizza(selectedSize, selectedToppings, selectedNumber);
-   yourOrder.pizzasOrdered.push(newPizza);
-   console.log(newPizza);
    console.log(yourOrder);
+
+   console.log(yourOrder.totalCost());
   //would like to have  pizza added to order displayed. maybe as a list
   });
-    });
-//
 
+  $(".add-another-pizza").click(function(){
+    $("#pizza-creation").append('<div class="pizza-Creator">' +
+                                  '<div>' +
+                                    '<h4> What size Pie would you like?</h4>' +
+                                    '<select class="form-control" id="pieSize">' +
+                                      '<option value="small" selected>Small (10 inch, $8)</option>' +
+                                      '<option value="medium">Medium (12 inch, $10)</option>' +
+                                      '<option value="large">Large (14 inch, $12)</option>' +
+                                      '<option value="xLarge">Extra Large (16 inch, $14)</option>' +
+                                    '</select>' +
+                                  '</div>' +
+                                  '<div class="toppings">' +
+                                    '<h4>Pick Your Toppings ($.50 each)</h4>' +
+                                    '<label class="checkbox-inline">' +
+                                    '<input type="checkbox"name="toppingCheck"  id="inlineCheckbox1" value="Extra Cheese"> Extra Cheese' +
+                                    '</label>' +
+                                    '<label class="checkbox-inline">' +
+                                    '<input type="checkbox" name="toppingCheck" id="inlineCheckbox2" value="Pepperoni"> Pepperoni' +
+                                    '</label>' +
+                                    '<label class="checkbox-inline">' +
+                                    '<input type="checkbox"name="toppingCheck" id="inlineCheckbox3" value="Onion"> Onions' +
+                                    '</label>' +
+                                    '<label class="checkbox-inline">' +
+                                    '<input type="checkbox"name="toppingCheck"  id="inlineCheckbox4" value="Mushrooms"> Mushrooms' +
+                                    '</label>' +
+                                    '<label class="checkbox-inline">' +
+                                    '<input type="checkbox"name="toppingCheck"  id="inlineCheckbox5" value="Green Peppers"> Green Peppers' +
+                                    '</label>' +
+                                  '</div>'+
+                                  '<div >' +
+                                    '<label for="numberPies"><strong>How Many?:</strong></label>' +
+                                    '<input id="numberPies" type="number" value = 1>' +
+                                  '</div>' +
+                                '</div> Want Another?');
+                          });
 
-
-  // });
+   });
 //
 // //
 // // //would like to display total order cost if possible as the orders are added
